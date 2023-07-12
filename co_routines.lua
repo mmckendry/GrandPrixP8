b23c69de882ce9e7c183976068fd3f14e69236fa
mmckendry <martinmckendry@live.ie>
2023-07-12 15:35:31 +0100

    Add co-routines example, fix only one entity moving at a time

---
+++ co_routines.lua
@@ -0,0 +1,30 @@
+local player_movement =function ()
+    for i=1,50 do
+      print("coroutine 1 " ..i)
+      coroutine.yield()
+    end
+  end
+
+  local opponent_movement = function ()
+    for i = 1,10 do
+      print("coroutine 2: " ..i)
+      coroutine.yield()
+    end
+  end
+
+  PLAYER = coroutine.create(player_movement)
+  OPPONENT = coroutine.create(opponent_movement)
+
+  for i = 1, 100 do
+    if (coroutine.status(OPPONENT) == "suspended" or coroutine.status(OPPONENT) == "dead") then
+      coroutine.resume(PLAYER)
+    end
+    if (coroutine.status(PLAYER) == "suspended") then
+      coroutine.resume(OPPONENT)
+    end
+  end
+
+-- Brief
+-- Two seperate methods where an entity is moving
+-- The first moves then yields and the second moves.
+-- Continue until there is a break (50 moves)
